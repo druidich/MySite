723e2bbf198fb64c85780de81c97001cf6e0f874
Mihayl <druidich92@yandex.ru>
2015-12-19 21:59:12 +0500

    its working !!!

--- run.py
+++ run.py
@@ -5,21 +5,22 @@ from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
-migrate = Migrate(app, db)
-
-
-def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
-
-
-def test():
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
+# manager = Manager(app)
+# migrate = Migrate(app, db)
+#
+#
+# def make_shell_context():
+#     return dict(app=app, db=db, User=User, Role=Role)
+#
+#
+# def test():
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
+#
+#
+# manager.add_command("shell", Shell(make_context=make_shell_context))
+# manager.add_command('db', MigrateCommand)
 if __name__ == '__main__':
-    manager.run()
+    # manager.run()
+    app.run()
